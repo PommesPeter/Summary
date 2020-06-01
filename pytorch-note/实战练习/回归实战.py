531372fe2dc07741bdaece6baf42f11b122b7d32
PommesPeter <434596665@qq.com>
2020-06-01 16:10:35 +0800

    add some code

--- pytorch-note/实战练习/回归实战.py
+++ pytorch-note/实战练习/回归实战.py
@@ -17,8 +17,7 @@ def compute_error_for_line_give_points(b, w, points):  # 计算损失
         x = points[i, 0]
     y = points[i, 1]
     totalError += (y - (w * x + b)) ** 2
-
-return totalError / float(len(points))
+    return totalError / float(len(points))
 
 
 # 计算梯度
@@ -47,17 +46,20 @@ def gradient_decent_runner(points, starting_b, starting_w,
 
     return [b, w]
 
+
 def run():
     points = np.genfromtxt('data.csv', delimiter=',')
     learning_rate = 0.0001
     init_b = 0
     init_w = 0
     num_iterations = 100
-    print('Starting gradient descent at b = %f, w = %f, error =%f' % (init_b, init_w, compute_error_for_line_give_points(init_b, init_w, points)))
+    print('Starting gradient descent at b = %f, w = %f, error =%f' % (
+    init_b, init_w, compute_error_for_line_give_points(init_b, init_w, points)))
     print('Running...')
     [b, w] = gradient_decent_runner(points, init_b, init_w, learning_rate, num_iterations)
-    print('After %d iterations b = %f, w = %f, error = %f' % (num_iterations, b ,w, compute_error_for_line_give_points(b, w, points)))
+    print('After %d iterations b = %f, w = %f, error = %f' % (
+    num_iterations, b, w, compute_error_for_line_give_points(b, w, points)))
 
 
-if __name___ = "__main__":
+if __name__ == '__main__':
     run()
