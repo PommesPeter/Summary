87c2a151c1167f3609511b10a305f79a2316e634
PommesPeter <434596665@qq.com>
2020-06-02 18:52:41 +0800

    write the code of download the dataset

--- pytorch-note/实战练习/分类问题-手写识别数字/手写识别数字.py
+++ pytorch-note/实战练习/分类问题-手写识别数字/手写识别数字.py
@@ -1,6 +1,20 @@
 import torch
 import torchvision
 import numpy as np
+from utils import *
 import torch.nn as nn
-from torch.autograd import *
+from torch import optim
+import matplotlib.pyplot as plt
+from torch.nn import functional as f
 
+learning_rate = 0.01
+batch_size = 512
+
+transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+                                             torchvision.transforms.Normalize((0.1307,), (0.3081,))])
+
+train_data = torchvision.datasets.MNIST('./data', train=True, transform=transforms, download=True)
+test_data = torchvision.datasets.MNIST('./data', train=False, transform=transforms, download=True)
+
+train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+train_dataloader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True)
